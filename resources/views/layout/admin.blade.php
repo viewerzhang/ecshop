abdd358ac8ae752d4fa7d063225565f4c2e76c35
lamp295367893 <295367893@qq.com>
2019-02-22 20:32:43 +0800

    修正了后台样式

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -13,29 +13,29 @@ Purchase: http://wrapbootstrap.com
     <meta name="description" content="Dashboard" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="shortcut icon" href="/static/admin/assets/img/favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('/static/admin/assets/img/favicon.png') }}" type="image/x-icon">
 
 
     <!--Basic Styles-->
     
-    <link href="/static/admin/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{ asset('/static/admin/assets/css/bootstrap.min.css') }}" rel="stylesheet" />
     <link id="bootstrap-rtl-link" href="" rel="stylesheet" />
-    <link href="/static/admin/assets/css/font-awesome.min.css" rel="stylesheet" />
-    <link href="/static/admin/assets/css/weather-icons.min.css" rel="stylesheet" />
+    <link href="{{ asset('/static/admin/assets/css/font-awesome.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/static/admin/assets/css/weather-icons.min.css') }}" rel="stylesheet" />
 
     <!--Fonts-->
 <!--     <link href="http://fonts.useso.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,600,700,300" rel="stylesheet" type="text/css"> -->
 
     <!--Beyond styles-->
     
-    <link href="/static/admin/assets/css/beyond.min.css" rel="stylesheet" />
-    <link href="/static/admin/assets/css/demo.min.css" rel="stylesheet" />
-    <link href="/static/admin/assets/css/typicons.min.css" rel="stylesheet" />
-    <link href="/static/admin/assets/css/animate.min.css" rel="stylesheet" />
+    <link href="{{ asset('/static/admin/assets/css/beyond.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/static/admin/assets/css/demo.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/static/admin/assets/css/typicons.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/static/admin/assets/css/animate.min.css') }}" rel="stylesheet" />
     <link id="skin-link" href="" rel="stylesheet" type="text/css" />
 
     <!--Skin Script: Place this script in head to load scripts for skins and rtl support-->
-    <script src="/static/admin/assets/js/skins.min.js"></script>
+    <script src="{{ asset('/static/admin/assets/js/skins.min.js') }}"></script>
 </head>
 <!-- /Head -->
 <!-- Body -->
